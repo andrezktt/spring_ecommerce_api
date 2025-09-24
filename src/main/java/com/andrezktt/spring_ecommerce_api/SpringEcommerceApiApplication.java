d14902c8793d969f26a129e45daab3bb78c5f9ed
andre_zktt <andrezicatti@gmail.com>
2025-09-24 13:01:07 -0300

    :tada: init: create project

---
+++ src/main/java/com/andrezktt/spring_ecommerce_api/SpringEcommerceApiApplication.java
@@ -0,0 +1,13 @@
+package com.andrezktt.spring_ecommerce_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEcommerceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEcommerceApiApplication.class, args);
+	}
+
+}
